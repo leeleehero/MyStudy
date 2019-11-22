1915e33743271954e0299187a08f110eb7b71467
leeleehero@163.com <leeleehero@163.com>
2019-11-22 09:11:07 +0800

    ''

--- MyStudy/src/lee/concurrent/pool/Demo1.java
+++ MyStudy/src/lee/concurrent/pool/Demo1.java
@@ -6,6 +6,8 @@ public class Demo1 {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         LinkedBlockingDeque<Runnable> runnables = new LinkedBlockingDeque<>();
         ThreadPoolExecutor executor = new ThreadPoolExecutor(3,10,3L, TimeUnit.SECONDS,runnables);
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
         for (int i = 0; i <20 ; i++) {
             Future<?> submit = executor.submit(() ->  1);
             System.out.println(submit.get());
